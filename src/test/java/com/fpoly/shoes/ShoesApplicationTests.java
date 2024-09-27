276d886a914425a6c51b814fb89c8fb21c23995e
yttnpc08086 <166134557+yttnpc08086@users.noreply.github.com>
2024-09-27 16:46:42 +0700

    build src

---
+++ src/test/java/com/fpoly/shoes/ShoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpoly.shoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
